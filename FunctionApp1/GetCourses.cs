519acc51511a1885696a3bd03e360ee32e536a13
Manish Myakal <Manish.Myakal@meijer.com>
2023-06-21 07:26:12 -0400

    removed filter in sp

--- FunctionApp1/GetCourses.cs
+++ FunctionApp1/GetCourses.cs
@@ -16,6 +16,7 @@ using System.Reflection.Metadata.Ecma335;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace FunctionApp1
 {
@@ -160,8 +161,8 @@ namespace FunctionApp1
                 //        Content = new StringContent(er.Serialize(), Encoding.UTF8, Constants.ContentType)
           //} :
                   return  new HttpResponseMessage(HttpStatusCode.OK)
-                    {
-                        Content = new StringContent(JsonConvert.SerializeObject(_lst), Encoding.UTF8, Constants.ContentType)
+                  {
+                        Content = new StringContent(JsonConvert.SerializeObject(_lst), Encoding.UTF8, "application/json")
                     };
 
 
